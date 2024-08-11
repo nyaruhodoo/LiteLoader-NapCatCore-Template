89f4c821ffd1ae9fe3e228694a3063b2a2a2725f
Nya <642886174@qq.com>
2024-08-11 17:54:06 +0800

    feat: 瞎加点东西

--- src/main/enum/eventsEnum.ts
+++ src/main/enum/eventsEnum.ts
@@ -5,24 +5,24 @@ export enum Events {
   // 请求广告?
   requestTianshuAdv = 'NodeIQQNTWrapperSession.getMsgService.requestTianshuAdv',
 
+  // 登录成功，能拿到 uin,uid
+  onQRCodeLoginSucceed = 'NodeIKernelLoginListener.onQRCodeLoginSucceed',
+
   // 个人信息发生变更，能拿到完整的用户数据
   onUserDetailInfoChanged = 'NodeIKernelProfileListener.onUserDetailInfoChanged',
 
   // 初始化获取个人数据(无nickName)
   onSelfStatusChanged = 'NodeIKernelProfileListener.onSelfStatusChanged',
 
-  // 本地登陆过的用户
+  // 本地登陆过的用户(uid，uid，nickname全拿得到)
   getLoginList = 'NodeIKernelLoginService.getLoginList',
-
-  // 初始化的时候可以获取到用户名(好像是qq小世界的...)
-  onRefreshGuildUserProfileInfo = 'NodeIKernelGuildListener.onRefreshGuildUserProfileInfo',
 
   // 收到新消息
   onRecvMsg = 'NodeIKernelMsgListener.onRecvMsg',
-
-  // 最近联系人更改，随着收到新消息一起触发
+  // 最近联系人更改
   onRecentContactListChangedVer2 = 'NodeIKernelRecentContactListener.onRecentContactListChangedVer2',
-
-  // 收到新消息后的系统通知，随着收到新消息一起触发
-  onRecentContactNotification = 'NodeIKernelRecentContactListener.onRecentContactNotification'
+  // 收到新消息后的系统通知
+  onRecentContactNotification = 'NodeIKernelRecentContactListener.onRecentContactNotification',
+  // 未读消息更新
+  onContactUnreadCntUpdate = 'NodeIKernelMsgListener.onContactUnreadCntUpdate'
 }
