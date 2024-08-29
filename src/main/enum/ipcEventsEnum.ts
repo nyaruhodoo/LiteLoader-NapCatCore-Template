f42460fabfe41a5a15f95e3c010340961d77e1ff
Nya <642886174@qq.com>
2024-08-29 12:10:06 +0800

    fix: 瞎鸡儿改

--- src/main/enum/ipcEventsEnum.ts
+++ src/main/enum/ipcEventsEnum.ts
@@ -1,6 +1,4 @@
 export const enum IPCEventsEnum {
   // 收到新消息(只会收到已激活窗口消息)
-  onRecvActiveMsg = 'nodeIKernelMsgListener/onRecvActiveMsg',
-  // 激活聊天窗口
-  activeMsg = 'nodeIKernelMsgService/getAioFirstViewLatestMsgsAndAddActiveChat'
+  onRecvActiveMsg = 'nodeIKernelMsgListener/onRecvActiveMsg'
 }
