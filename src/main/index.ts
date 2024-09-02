36d7e47df917e3ac9e6d06c642734167ec155303
Nya <642886174@qq.com>
2024-09-02 17:40:13 +0800

    fix: 移除测试代码

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,13 +1,10 @@
 import { hookWrapper } from '@/main/hook/hookWrapper'
 import { EventEnum } from './enum/eventEnum'
-import { initForward } from './forward'
 ;(async () => {
   await hookWrapper({
     eventBlacklist: [EventEnum.sendLog, EventEnum.requestTianshuAdv],
-    log: true
+    log: false
   })
-
-  initForward()
 })()
 
 // Hook IPC 必须在 onBrowserWindowCreated 中调用
